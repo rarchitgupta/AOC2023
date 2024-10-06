59df406e8f1d664477a691e5b65d43d55a0e8ec0
Archit Gupta <rarchitgupta@gmail.com>
2024-10-06 00:59:18 -0400

    Changed filename to allow for inclusion of test 2 file

--- Day 1/test.py
+++ Day 1/test.py
@@ -3,7 +3,7 @@ import main
 
 class TestDay1(unittest.TestCase):
     def test_part_1(self):
-        self.assertEqual(main.part_1("test.txt"), 142)
+        self.assertEqual(main.part_1("test_part1.txt"), 142)
 
 
 if __name__ == '__main__':
